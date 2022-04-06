f18cbe2d00c863c3463bf934cc9af2c4f6c23d45
FelipeRojas <jrojasce@unal.edu.co>
2022-04-06 16:55:53 -0500

    structure finished, cinema quiz started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { EntertainmentComponent } from './entertainment/entertainment.component';
 
 const routes: Routes = [
   {
     path: 'home', 
     component: HomeComponent
+  },
+  {
+    path: 'news',
+    component: NewsComponent
+  },
+  {
+    path: 'entertainment', 
+    component: EntertainmentComponent
   },
   { 
     path: '', 
